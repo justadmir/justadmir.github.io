2c281eadb58e907f7959d9ce25d9f7f36e065f42
justadmir <115950510+justadmir@users.noreply.github.com>
2024-09-12 23:55:13 +0200

    Update install-plist.php

--- dl/enterprise/install-plist.php
+++ dl/enterprise/install-plist.php
@@ -4,12 +4,18 @@ $app = $_GET['app'];
 
 // Check if the 'app' parameter is set
 if (isset($app) && !empty($app)) {
-  // Construct the redirect URL
-  $redirectUrl = "itms-services://?action=download-manifest&url=$app";
-  
-  // Redirect the user to the constructed URL
-  header("Location: $redirectUrl");
-  exit;
+  // Check if the 'app' parameter ends with '.plist'
+  if (substr($app, -6) !== '.plist') {
+    // If it doesn't end with '.plist', display an error message
+    echo "Error: 'app' parameter must end with '.plist'.";
+  } else {
+    // Construct the redirect URL
+    $redirectUrl = "itms-services://?action=download-manifest&url=$app";
+    
+    // Redirect the user to the constructed URL
+    header("Location: $redirectUrl");
+    exit;
+  }
 } else {
   // If the 'app' parameter is not set, display an error message
   echo "Error: 'app' parameter is required.";
