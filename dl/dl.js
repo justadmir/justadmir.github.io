f2f67cc598da15c353cbe09499ead94154243e46
justadmir <115950510+justadmir@users.noreply.github.com>
2024-09-13 00:26:55 +0200

    Delete dl directory

--- dl/dl.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', function(event) {
-            event.preventDefault();
-            var button = this;
-            button.classList.add('downloading');
-            button.innerHTML = 'Downloading...';
-            setTimeout(function() {
-                window.location.href = button.href;
-            }, 2000); // Simulate a delay for downloading
-        });
